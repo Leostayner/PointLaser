84d9b6e2ef0e22f452023503349d9f64361d7e47
Gabriel Moreira <gabrielfm2@al.insper.edu.br>
2017-11-28 14:00:24 -0200

    pid

--- d/d.ino
+++ d/d.ino
@@ -1,9 +1,13 @@
+#include <PID_v1.h>
 
-#include <Servo.h> 
-Servo myservo;  // create servo object to control a servo 
+double Setpoint;
+double Input;
+double Output;
+PID myPID(&Input, &Output, &Setpoint, 0.4, 0, 0, DIRECT);
+
 
 // Pino analgico do potenciometro
-int potpin = 0;
+int potpin = A0;
 
 // Variavel que armazena o valor lido do potenciometro
 int val;     
@@ -16,25 +20,36 @@ int count;
 
 
 void setup() 
-{ 
+{
+  Setpoint = 920;
+  Input = analogRead(0);
   // Define que o servo esta ligado a porta 9
   Serial.begin(9600);      // open the serial port at 9600 bps:
   pinMode(3, OUTPUT);          // sets the digital pin 13 as output
   pinMode(5, OUTPUT);          // sets the digital pin 13 as output
   count = 0;
   int number;
+  myPID.SetMode(AUTOMATIC);
   
 }
 
 void loop(){
-  int pin;
-  bool busy = false;
-  char myString;
-  int n = 0;
   // Le o valor do potenciometro (valores entre 0 e 1023) 
-  val = analogRead(potpin);
+  Input = analogRead(potpin);
   number = 0;
-  Serial.print(val);
+  myPID.Compute();
+  Serial.print(Output);
+  if (Output< 0){
+    pin = 5;
+    Output = -Output;
+    }
+  else{
+    pin = 3;
+    }
+  analogWrite(pin, Output);
+  delay(20);
+  analogWrite(pin, 0); 
+
   
   // send data only when you receive data:
   if (Serial.available() > 0) {
@@ -44,24 +59,23 @@ void loop(){
             count = 0;
             number = atoi(buffer);
             if(number < 0){
-              Serial.write('n');
+//              Serial.write('n');
               pin = 5;
+              number = -number;
             }
             else{
-              Serial.write('p');
+//              Serial.write('p');
               pin = 3;
             }
-            number = map(number, -360, 360, 0, 255);     // scale it to use it with the servo (value between 0 and 180)
-            analogWrite(pin, number);
-            delay(20);
-            analogWrite(pin, 0); 
+            
+            number = map(number, -360, 360, 0, 255);
+//            analogWrite(pin, number);
+//            delay(20);
+//            analogWrite(pin, 0); 
         }
         else if (count < sizeof buffer - 1) {
             buffer[count++] = c;
         }
-
-
-
 }}
 
 
